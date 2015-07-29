1213c9329b804a575a103328c8c731179e007ddd
Robin Burton <1.robinburton@gmail.com>
2015-07-29 15:39:44 -0500

    added words for translation

--- spanishsection.js
+++ spanishsection.js
@@ -1,5 +1,9 @@
 var Translate = (function(translate) {
- var dictionary = {"merry":"alegre", "christmas":"navidad", "and":"y", "happy":"feliz", "new":"nuevo", "year":"ano"};
+ var dictionary = {"merry":"alegre", "christmas":"navidad", "and":"y", "happy":"feliz", "new":"nuevo",
+  "year":"ano", "holidays":"vacaciones", "hanukkah":"januca", "kwanza":"kwanza", "winter":"invierno",
+   "we":"nosotros", "wish":"desear", "you":"ustedes", "a":"la", "seasons":"estaciones", "greetings":"saludos",
+    "santa":"de santa", "deck":"cubierta", "the":"la", "halls":"salones", "holly":"acebo", "jolly":"alegre",
+     "love":"amor", "joy":"alegría", "peace":"paz", "noel":"noel"};
 
  
  	translate.translateToSpanish = function(sentence) {
