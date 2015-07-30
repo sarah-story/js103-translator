be2f14db0801f59285f9652b2ef90aad2c4d3f24
Sarah Story <sarah.f.lavendol@vanderbilt.edu>
2015-07-30 09:36:34 -0500

    Added language support for text-to-speech

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+//function executed when translate button is clicked
+
 $('button').click( function() {
   
+  //get input values from textarea and select box
   var sentence = $("#input").val();
   var language = $("#language").val();
-  
+  $('#results').css('display': 'block');
+
+  //if statements to execute based on language selected
   if (language === 'italian') {
    
+    //pass sentence input into translate function
     var italianSentence = Translate.translateToItalian(sentence);
+
+    //write results to page
     $('#results').text(italianSentence);
+
+    //text-to-speech function (only works in chrome)
     var msg = new SpeechSynthesisUtterance(italianSentence);
+    var voices = window.speechSynthesis.getVoices();
+    msg.lang = 'it';
     window.speechSynthesis.speak(msg);
   
   } else if (language === 'finnish') {
@@ -15,6 +27,8 @@ $('button').click( function() {
     var finnishSentence = Translate.translateToFinnish(sentence);
     $('#results').text(finnishSentence);
     var msg = new SpeechSynthesisUtterance(finnishSentence);
+    var voices = window.speechSynthesis.getVoices();
+    msg.lang = 'fi';
     window.speechSynthesis.speak(msg);
   
   } else {
@@ -22,6 +36,8 @@ $('button').click( function() {
     var spanishSentence = Translate.translateToSpanish(sentence);
     $('#results').text(spanishSentence);
     var msg = new SpeechSynthesisUtterance(spanishSentence);
+    var voices = window.speechSynthesis.getVoices();
+    msg.lang = 'es';
     window.speechSynthesis.speak(msg);
   
   }
--- finnish.js
+++ finnish.js
@@ -1,25 +1,39 @@
+//A function augmenting italian.js
+
 var Translate = (function(translate) {
   var dictionary = {'merry':'iloinen', 'christmas':'joulu', 'and':'ja', 'happy':'onnellinen', 'new':'uusi', 'year':'vuosi',
 'holidays':'juhlapaivat', 'hanukkah': 'hanukka', 'kwanza':'kwanza', 'winter':'talvi', 'we':'me', 'wish':'haluta', 'you':'te', 'a':'a', 
 'seasons':'vuodenajat', 'greetings':'tervehdys', 'santa': 'joulupukki', 'deck':'kansi', 'the':'the', 'halls':'hallit', 
 'holly':'orjanlaakeri', 'jolly':'hilpeä', 'love':'rakkaus', 'joy':'ilo', 'peace':'rauha', 'noel':'noel'};
 
+  //Adding a transate to finnish function to the Translate function
   translate.translateToFinnish = function(sentence) {
+    //Split input into an array
     var sentenceArray = sentence.toLowerCase().split(" ");
-      var finnishSentence = "";
-      for (var i = 0; i < sentenceArray.length; i++) {
-        if (sentenceArray[i] in dictionary) {
 
-          finnishSentence += dictionary[sentenceArray[i]] + " ";
+    //Initialize sentence as an empty sctring
+    var finnishSentence = "";
 
-        } else {
+    //loop through sentence array
+    for (var i = 0; i < sentenceArray.length; i++) {
 
-          alert("Sorry, we don't recognize the word: " + sentenceArray[i] + ". Please try again!")
-          finnishSentence = '';
+      //check to see if word is in dictionary
+      if (sentenceArray[i] in dictionary) {
 
-        }
+        //add Finnish translation of word to the finnish sentence string
+        finnishSentence += dictionary[sentenceArray[i]] + " ";
+
+      } else {
+
+        //alert user if word is not in dictionary
+        alert("Sorry, we don't recognize the word: " + sentenceArray[i] + ". Please try again!")
+        //set reset sentence to an empty string
+        finnishSentence = '';
+        //end loop
+        break;
       }
-      return finnishSentence;
+    }
+    return finnishSentence;
   }
 
   return translate;
